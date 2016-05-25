dd2f990d604edfbed3aad9d6930be7dc81c4766e
Guruprasad Kulkarni <guru@linux.com>
2016-05-25 09:35:15 +0000

    add url path parameters

--- flask_init/hello.py
+++ flask_init/hello.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World From Flask!!!!! :D'
+
+@app.route('/users/<username>')
+def say_hi_to_user(username):
+    return 'Hey there.... %s' % username
+
+@app.route('/user/<int:userid>')
+def get_user_by_id(userid):
+    return 'user id entered %d' % userid
     
 if __name__ == '__main__':
     host = os.getenv('IP', '0.0.0.0')
